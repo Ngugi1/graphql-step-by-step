1bb257550541bbc39ecffa00e63b5640f231b513
Sam Ngugi😀 <ngugindunguapps@gmail.com>
2018-08-22 10:14:55 +0300

    Add src/index.js and import both prisma-bindings and graphql-yoga

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Import required modules 
+
+const { GraphQLServer } = require('graphql-yoga');
+
+// Prisma-bindings will help you access the database using JS functions
+const { Prisma  } = require('prisma-binding'); 
+
