f8cf6f0c6aecc8c232c0446e856f83e5d6503bf2
Sam Ngugi😀 <ngugindunguapps@gmail.com>
2018-08-23 09:22:15 +0300

    Instanciate prisma

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 const {GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 const resolvers = {
     Query: {
-        info: () => {
-            return {
-                url: "http://localhost",
-                description: "Sample"
-            };
+        info: () => "This is the hackernews node API",
+        feed: (root, args,context,info) => {
+            return context.db.query.links({}, info);
+        }
+    },
+
+    Mutation: {
+        post: (root, args, context, info) => {
+            return context.db.mutation.createLink({
+                data: {
+                    url: args.url,
+                    description: args.description
+                }
+            }, info);
         }
     }
 };
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers,
-})
+    resolvers: resolvers,
+    context: new Prisma({
+        typeDefs: 'src/generated/prisma.graphql',
+        endpoint: 'https://us1.prisma.sh/ngugi-ndungu-ca026f/graphql-step-by-step-prismadb/practice',
+        secret: 'graphql-step-by-step',
+        debug: true
+    })
+});
 
-server.start(() => console.log("Server is running on port 4000"));
+server.start(function () {console.log("Server is running on port 4000");});
